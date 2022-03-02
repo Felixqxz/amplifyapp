c09a60e63acd24a0e1a72f00b044ae085a55a7b7
Xingzhi Qian <40925246+Felixqxz@users.noreply.github.com>
2022-03-02 20:53:57 +0000

    add image storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,18 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
+    },
+    "api": {
+        "amplifyapp": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "storage": {
+        "s34124884d": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
--- src/App.js
+++ src/App.js
@@ -1,18 +1,99 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import { Authenticator, withAuthenticator } from '@aws-amplify/ui-react'
+import logo from './logo.svg'
+import { API, Storage } from 'aws-amplify';
+import { Authenticator} from '@aws-amplify/ui-react'
+import { listTodos } from './graphql/queries';
+import { createTodo as createTodoMutation, deleteTodo as deleteTodoMutation } from './graphql/mutations';
+
+const initialFormState = { name: '', description: '' }
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [formData, setFormData] = useState(initialFormState);
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  async function fetchTodos() {
+    const apiData = await API.graphql({ query: listTodos });
+    const notesFromAPI = apiData.data.listNotes.items;
+    await Promise.all(notesFromAPI.map(async note => {
+      if (note.image) {
+        const image = await Storage.get(note.image);
+        note.image = image;
+      }
+      return note;
+    }))
+    setTodos(apiData.data.listNotes.items);
+  }
+
+  async function createTodo() {
+    if (!formData.name || !formData.description) return;
+    await API.graphql({ query: createTodoMutation, variables: { input: formData } });
+    if (formData.image) {
+      const image = await Storage.get(formData.image);
+      formData.image = image;
+    }
+    setTodos([ ...todos, formData ]);
+    setFormData(initialFormState);
+  }
+
+  async function deleteTodo({ id }) {
+    const newNotesArray = todos.filter(note => note.id !== id);
+    setTodos(newNotesArray);
+    await API.graphql({ query: deleteTodoMutation, variables: { input: { id } }});
+  }
+
+  async function onChange(e) {
+    if (!e.target.files[0]) return
+    const file = e.target.files[0];
+    setFormData({ ...formData, image: file.name });
+    await Storage.put(file.name, file);
+    fetchTodos();
+  }
+
   return (
     <Authenticator>
       {({ signOut, user }) => 
         <div className="App">
+          
           <header>
             <img src={logo} className="App-logo" alt="logo" />
             <h1>We now have Auth!</h1>
           </header>
           <button onClick={signOut}>Sign Out</button>
+          <h1>My Notes App</h1>
+          <input
+            onChange={e => setFormData({ ...formData, 'name': e.target.value})}
+            placeholder="Note name"
+            value={formData.name}
+          />
+          <input
+            onChange={e => setFormData({ ...formData, 'description': e.target.value})}
+            placeholder="Note description"
+            value={formData.description}
+          />
+          <input
+            type="file"
+            onChange={onChange}
+          />
+          <button onClick={createTodo}>Create Note</button>
+          <div style={{marginBottom: 30}}>
+            {
+              todos.map(note => (
+                <div key={note.id || note.name}>
+                  <h2>{note.name}</h2>
+                  <p>{note.description}</p>
+                  <button onClick={() => deleteTodo(note)}>Delete note</button>
+                  {
+                    note.image && <img src={note.image} style={{width: 400}} />
+                  }
+                </div>
+              ))
+            }
+          </div>
         </div>
       }
     </Authenticator>
